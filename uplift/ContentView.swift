28d9a7f2fb2039d0676ab62a13e266308fc436cd
Ruitao Chen <ruitaocchen@gmail.com>
2025-12-02 14:18:23 -0500

    Swift Creation

---
+++ uplift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  uplift
+//
+//  Created by Ruitao Chen on 12/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ uplift/upliftApp.swift
@@ -0,0 +1,17 @@
+//
+//  upliftApp.swift
+//  uplift
+//
+//  Created by Ruitao Chen on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct upliftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
